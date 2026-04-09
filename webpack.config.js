0c5a6acffcea83581905cb911a25bd117b6784b0
Abhishek.Verma <axvrma@gmail.com>
2026-04-09 09:47:29 +0530

    netlify support

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,15 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 module.exports = {
-    mode: 'development',
+    mode: isProduction ? 'production' : 'development',
     entry: './src/index.js',
     output: {
-        filename: 'bundle.js',
+        filename: isProduction ? 'bundle.[contenthash].js' : 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true,
     },
     module: {
         rules: [
